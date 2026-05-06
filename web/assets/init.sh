4e475b101b7463829d248314eaaa697f4dbec266
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2026-05-06 08:51:56 +0800

    Add CodeMirror integration in index.html for enhanced TOML configuration editing

---
+++ web/assets/init.sh
@@ -0,0 +1,7 @@
+rm toml.min.js
+rm codemirror.min.js
+rm codemirror.min.css
+
+wget https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.16/mode/toml/toml.min.js
+wget https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.16/codemirror.min.js
+wget https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.16/codemirror.min.css
